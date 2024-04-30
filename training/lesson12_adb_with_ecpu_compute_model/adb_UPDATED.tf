10cd81e6010380d05fa5f2e5d03d64ce04fbe012
mlinxfeld <martin.linxfeld@foggykitchen.com>
2024-04-30 11:11:33 +0000

    lesson12

---
+++ training/lesson12_adb_with_ecpu_compute_model/adb_UPDATED.tf
@@ -0,0 +1,18 @@
+module "oci-fk-adb" {
+  source                                = "github.com/mlinxfeld/terraform-oci-fk-adb"
+  adb_database_db_name                  = "FoggyKitchenADB"
+  adb_database_display_name             = "FoggyKitchenADB"
+  adb_password                          = var.adb_password
+  adb_database_db_workload              = "OLTP" # Autonomous Transaction Processing (ATP)
+  adb_free_tier                         = false
+  #adb_database_cpu_core_count           = 1
+  adb_database_data_storage_size_in_tbs = 1
+  compartment_ocid                      = var.compartment_ocid
+  use_existing_vcn                      = false
+  adb_private_endpoint                  = true
+  adb_compute_model                     = "ECPU"
+  adb_compute_count                     = 2
+  is_auto_scaling_enabled               = true
+  is_auto_scaling_for_storage_enabled   = true
+}
+
